6320fbb2c4bfa6abfe87732542ad8af04da02484
Sahil Patel <sahildepatel@gmail.com>
2022-09-23 22:09:29 -0400

    Adidng logo and navbar

---
+++ esquina-cubana/src/components/header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+export default class Header extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      
+      <header id="home">
+         <nav id="nav-wrap">
+            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+            <ul id="nav" className="nav">
+               <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+               <li><a className="smoothscroll" href="#about">About</a></li>
+             <li><a className="smoothscroll" href="#resume">Resume</a></li>
+               <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+               <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            </ul>
+         </nav>
+      </header>
+      </React.Fragment>
+    );
+  }
+}
